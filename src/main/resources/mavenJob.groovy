4a03c8a279b7b13f584d5294aaa4497ad438ee58
Charles Moulliard <ch007m@gmail.com>
2020-12-17 11:07:01 +0100

    Remove the comment as not needed

--- src/main/resources/mavenJob.groovy
+++ src/main/resources/mavenJob.groovy
@@ -1,4 +1,3 @@
-// See: https://www.happycoders.eu/devops/jenkins-tutorial-create-jobs-with-job-dsl/
 mavenJob('mvn-spring-boot-rest-http') {
     description 'A Maven Job compiling the project Spring Boot Rest HTTP Example'
 
