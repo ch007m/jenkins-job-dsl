d70782e1ab7493f9da1ba8cca822ecd7c0ffcd01
Charles Moulliard <ch007m@gmail.com>
2020-12-21 18:42:46 +0100

    Add new test case supporting groovyCommand part of the DSL Job

---
+++ src/main/resources/checkBomDependenciesJob.groovy
@@ -0,0 +1,96 @@
+/**
+ * ---
+ - name: "Check BOM dependencies"
+ block:
+ - name: "Make backup of the pom file"
+ copy:
+ src: "{{ local_sd_gh_project_path }}/pom.xml"
+ dest: "{{ local_sd_gh_project_path }}/pom.xml.bak"
+
+ - name: "Replace dependency management in pom.xml"
+ replace:
+ path: "{{ local_sd_gh_project_path }}/pom.xml"
+ regexp: "{{ item }}"
+ replace: '\1\2'
+ loop:
+ - '(\s+)<dependencyManagement>(\s+.*)?$'
+ - '(\s+)</dependencyManagement>(\s+.*)?$'
+
+ - name: "Run maven dependency tree"
+ shell: "mvn dependency:tree"
+ args:
+ chdir: "{{ local_sd_gh_project_path }}"
+ register: dependency_tree
+
+ - name: "Save dependency info"
+ copy:
+ content: "{{ dependency_tree.stdout }}"
+ dest: "{{ local_release_folder }}/{{ snowdrop_version_full }}/upstream-dependency-tree.txt"
+
+ always:
+ - name: "Leave pom.xml as was"
+ copy:
+ src: "{{ local_sd_gh_project_path }}/pom.xml.bak"
+ dest: "{{ local_sd_gh_project_path }}/pom.xml"
+
+ - name: "Delete backup"
+ file:
+ path: "{{ local_sd_gh_project_path }}/pom.xml.bak"
+ state: absent
+
+ - name: "Get git status for folder"
+ shell: "git status"
+ args:
+ chdir: "{{ local_sd_gh_project_path }}"
+ register: git_status_res
+
+ - name: "Fail if there are local changes"
+ fail:
+ msg: "There are changes to the local repository ({{ local_sd_gh_project_path }}), cannot move on..."
+ when: "not ('nothing to commit, working tree clean' in git_status_res.stdout)"
+
+ ...
+
+ **/
+
+mavenJob('check-bom-dependencies') {
+    description 'Maven Job checking the BOM dependencies of the Spring Boot project'
+
+    logRotator {
+        numToKeep 3
+    }
+
+    parameters {
+        stringParam('TAG', '2.3.4.Final',
+                'The release version for the artifact. If you leave this empty, ' +
+                        'the current SNAPSHOT version will be used with the ' +
+                        '"-SNAPSHOT" suffix removed (example: if the current version ' +
+                        'is "1.0-SNAPSHOT", the release version will be "1.0").')
+    }
+
+    scm {
+        git {
+            remote {
+                url 'https://github.com/snowdrop/spring-boot-bom'
+                branch '$TAG'
+            }
+        }
+    }
+
+    preBuildSteps {
+            systemGroovyCommand '''\
+              import jenkins.model.Jenkins
+              import static groovy.io.FileType.*
+              import hudson.EnvVars
+              
+              def jenkins = Jenkins.getInstanceOrNull()
+              EnvVars envVars = build.getEnvironment(listener);
+              
+              def workspace = envVars.get('WORKSPACE')
+              println "WKS: $workspace"
+              '''.stripIndent()
+    }
+
+    rootPOM 'pom.xml'
+    goals '-B dependency:tree'
+}
---
+++ src/test/java/dev/snowdrop/MavenCheckBOMJobDSLTest.java
@@ -0,0 +1,81 @@
+package dev.snowdrop;
+
+import hudson.maven.MavenModuleSet;
+import hudson.maven.MavenModuleSetBuild;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.groovy.Groovy;
+import hudson.plugins.groovy.GroovyInstallation;
+import hudson.tasks.Maven;
+import javaposse.jobdsl.plugin.ExecuteDslScripts;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.ToolInstallations;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MavenCheckBOMJobDSLTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void useMavenDSLGroovyFileAsJob() throws Exception {
+        // Add maven to the Jenkins Global Configuration Tools as it is needed by the mavenJob
+        Maven.MavenInstallation mvn = ToolInstallations.configureDefaultMaven("apache-maven-3.6.3", Maven.MavenInstallation.MAVEN_30);
+        Maven.MavenInstallation m3 = new Maven.MavenInstallation("apache-maven-3.6.3", mvn.getHome(), JenkinsRule.NO_PROPERTIES);
+        j.getInstance().getDescriptorByType(Maven.DescriptorImpl.class).setInstallations(m3);
+
+        // Add Groovy to the Jenkins Global Configuration Tools as it is needed by the mavenJob
+        // systemGroovyCommand - child
+        // groovyCommand - master
+        GroovyInstallation groovy = new GroovyInstallation("groovy3","",null);
+        j.getInstance().getDescriptorByType(Groovy.DescriptorImpl.class).setInstallations(groovy);
+
+        FreeStyleProject job = j.createFreeStyleProject("maven-seed-job");
+        // Setup the ExecuteDslScripts to load the content of the DSL groovy script = mavenJob.groovy
+        ExecuteDslScripts e = new ExecuteDslScripts();
+        //e.setTargets("mavenJob.groovy");
+        String dslScript = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/checkBomDependenciesJob.groovy")))
+                .lines().collect(Collectors.joining("\n"));
+        e.setScriptText(dslScript);
+        e.setUseScriptText(true);
+        job.getBuildersList().add(e);
+
+        // Execute the seed job to create the mavenJob
+        FreeStyleBuild b = job.scheduleBuild2(0).get();
+
+        assertEquals(1, b.number);
+        assertEquals("#1", b.getDisplayName());
+        if (b.getResult().toString() != "SUCCESS") {
+            ArrayList LogResult = (ArrayList) b.getLog(200);
+            LogResult.forEach((s) -> System.out.println(s));
+        }
+
+        // Check if the FreeStyleProject build reported that it generated the job: say-hello-world
+        ArrayList LogResult = (ArrayList) b.getLog(100);
+        //LogResult.forEach((s) -> System.out.println(s));
+        assertTrue(b.getLog(100).stream().anyMatch(str -> str.contains("GeneratedJob{name='check-bom-dependencies'")));
+
+        // Get the FreeStyleProject containing the Job DSL steps to be executed - "check-bom-dependencies"
+        MavenModuleSet mavenJob = (MavenModuleSet) j.jenkins.getItem("check-bom-dependencies");
+        MavenModuleSetBuild b2 = mavenJob.scheduleBuild2(0).get();
+
+        assertEquals(1, b2.number);
+        assertEquals("#1", b2.getDisplayName());
+        if (b2.getResult().toString() != "SUCCESS") {
+            LogResult = (ArrayList) b2.getLog(200);
+            LogResult.forEach((s) -> System.out.println(s));
+        } else {
+            System.out.println("Job build succeeded !");
+            LogResult = (ArrayList) b2.getLog(200);
+            LogResult.forEach((s) -> System.out.println(s));
+        }
+    }
+}
