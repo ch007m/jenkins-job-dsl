8e5f236ec7dff6f1a2ce3f7bbca8ba200cb7f8b1
Charles Moulliard <ch007m@gmail.com>
2020-12-22 13:17:03 +0100

    Check if build = null when running locally

--- src/main/resources/backupPOM.groovy
+++ src/main/resources/backupPOM.groovy
@@ -1,17 +1,46 @@
-import jenkins.model.Jenkins
 import hudson.EnvVars
+import hudson.model.AbstractBuild
 import java.nio.file.Files
+import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
-def jenkins = Jenkins.getInstanceOrNull()
-EnvVars envVars = build.getEnvironment(listener);
+def workspace
+def AbstractBuild build
 
-def workspace = envVars.get('WORKSPACE')
+/*
+ * Print the workspace path
+ */
+if (build != null) {
+    EnvVars envVars = build.getEnvironment(listener);
+    workspace = envVars.get('WORKSPACE')
+} else {
+    workspace = "/Users/cmoullia/code/snowdrop/examples/bom"
+}
+
 println "WKS: $workspace"
-println "Backup pom.xml file"
-def file = new File("$workspace/pom.xml")
-def newFile = new File("$workspace/pom.xml.bk")
-Files.copy(file.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
 
+/*
+ * Backup the pom.xml file
+ */
+println "Backup pom.xml file"
+def pomfile = new File("$workspace/pom.xml")
+def pomBackupFile = new File("$workspace/pom.xml.bk")
+Files.copy(pomfile.toPath(), pomBackupFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+
+/*
+ * List the files within the job workspace
+ */
 println "List files within the Workspace of the job"
 "ls -la $workspace".execute().text
+
+/*
+ * Remove the <dependencyManagement>, </dependencyManagement> tags
+ */
+pomFileText = Files.readString(Paths.get(pomfile.toURI()));
+pomFileText.replaceFirst("<dependencyManagement>","")
+pomFileText.replaceFirst("</dependencyManagement>","")
+println("Pom modified : " + pomFileText)
+
+pomfile.write(pomFileText)
+println "Look if the pom file dont include the tags <dependencyManagement>, </dependencyManagement>"
+"cat $workspace/pom.xml".execute().text
