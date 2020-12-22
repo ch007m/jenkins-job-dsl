82ff9c00c9b728c35512827c0cc5827aedb19cf6
Charles Moulliard <ch007m@gmail.com>
2020-12-22 12:01:01 +0100

    Use java language instead of cp command

--- src/main/resources/backupPOM.groovy
+++ src/main/resources/backupPOM.groovy
@@ -1,5 +1,7 @@
 import jenkins.model.Jenkins
 import hudson.EnvVars
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 def jenkins = Jenkins.getInstanceOrNull()
 EnvVars envVars = build.getEnvironment(listener);
@@ -7,7 +9,11 @@ EnvVars envVars = build.getEnvironment(listener);
 def workspace = envVars.get('WORKSPACE')
 println "WKS: $workspace"
 println "Backup pom.xml file"
-"cp $workspace/pom.xml $workspace/pom.xml.bk".execute().text
+//"cp $workspace/pom.xml $workspace/pom.xml.bk".execute().text
+
+def file = new File("$workspace/pom.xml")
+def newFile = new File("$workspace/pom.xml.bk")
+Files.copy(file.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
 
 println "List files within the Workspace of the job"
 "ls -la $workspace".execute().text
