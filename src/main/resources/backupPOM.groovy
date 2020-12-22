1d9496a2800d59968fb5a66581709dde6946c242
Charles Moulliard <ch007m@gmail.com>
2020-12-22 15:06:57 +0100

    Fix the code to read the content of yhe file. As jenkins must run using JDK8, then the Files.readString() is not supported as part of jdk11

--- src/main/resources/backupPOM.groovy
+++ src/main/resources/backupPOM.groovy
@@ -1,46 +1,50 @@
-import hudson.EnvVars
-import hudson.model.AbstractBuild
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
+import hudson.EnvVars;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Collectors;
 
-def workspace
-def AbstractBuild build
+def workspace;
+def env = System.getenv();
 
 /*
  * Print the workspace path
  */
-if (build != null) {
+try {
     EnvVars envVars = build.getEnvironment(listener);
     workspace = envVars.get('WORKSPACE')
-} else {
-    workspace = args[0]
+} catch(groovy.lang.MissingPropertyException e) {
+    println ("Build is null");
+    workspace = env['MAVEN_PROJECT'];
 }
 
-println "WKS: $workspace"
+println "WKS: $workspace";
 
 /*
  * Backup the pom.xml file
  */
 println "Backup pom.xml file"
-File pomfile = new File("$workspace/pom.xml")
-File pomBackupFile = new File("$workspace/pom.xml.bk")
-Files.copy(pomfile.toPath(), pomBackupFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+File pomfile = new File("$workspace/pom.xml");
+File pomBackupFile = new File("$workspace/pom.xml.bk");
+Files.copy(pomfile.toPath(), pomBackupFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
 /*
  * List the files within the job workspace
  */
 println "List files within the Workspace of the job"
-"ls -la $workspace".execute().text
+"ls -la $workspace".execute().text;
 
 /*
  * Remove the <dependencyManagement>, </dependencyManagement> tags
  */
-String pomFileText = Files.readString(Paths.get(pomfile.toURI()));
-pomFileText = pomFileText.replaceFirst("<dependencyManagement>","")
-pomFileText = pomFileText.replaceFirst("</dependencyManagement>","")
+// Code is only working with jdk11
+//String pomFileText = Files.readString(Paths.get(pomfile.toURI()));
+String pomFileText = Files.lines(Paths.get(pomfile.toURI()))
+        .collect(Collectors.joining(System.lineSeparator()));
+pomFileText = pomFileText.replaceFirst("<dependencyManagement>","");
+pomFileText = pomFileText.replaceFirst("</dependencyManagement>","");
 //println("Pom modified : " + pomFileText)
 
-pomfile.write(pomFileText)
+pomfile.write(pomFileText);
 println "Look if the pom file dont include the tags <dependencyManagement>, </dependencyManagement>"
-"cat $workspace/pom.xml".execute().text
+"cat $workspace/pom.xml".execute().text;
