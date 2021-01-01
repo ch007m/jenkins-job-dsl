c87e4185e6c573190373550417084fd4cb63e2d3
Charles Moulliard <ch007m@gmail.com>
2021-01-01 13:58:15 +0100

    Load the pipeline using a resource grrovy file

--- src/test/java/dev/snowdrop/PipelineJobDSLTest.java
+++ src/test/java/dev/snowdrop/PipelineJobDSLTest.java
@@ -3,12 +3,16 @@ package dev.snowdrop;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.WithPlugin;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 
 public class PipelineJobDSLTest {
@@ -16,33 +20,18 @@ public class PipelineJobDSLTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
-    private String script = "pipeline {\n" +
-            "    agent any\n" +
-            "\n" +
-            "     stages {\n" +
-            "        stage('Prepare') {\n" +
-            "            steps {\n" +
-            "                echo 'Preparing ...'\n" +
-            "                sh 'mvn --version'\n" +
-            "            }\n" +
-            "        }\n" +
-            "        stage('Build') {\n" +
-            "            steps {\n" +
-            "                echo 'Building ...'\n" +
-            "            }\n" +
-            "        }\n" +
-            "        stage('Deploy') {\n" +
-            "            steps {\n" +
-            "                echo 'Deploying ...'\n" +
-            "            }\n" +
-            "        }\n" +
-            "    }\n" +
-            "}\n";
+    private String pipelineScript;
+
+    @Before
+    public void fetchPipelineGroovy() {
+        pipelineScript = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/pipelinegitMaven.groovy")))
+                .lines().collect(Collectors.joining("\n"));
+    }
 
     @WithPlugin({"durable-task-1.35.hpi"})
     @Test
     public void useSimpleDSLGroovyFileAsJob() throws Exception {
-        CpsFlowDefinition flow = new CpsFlowDefinition(script,false);
+        CpsFlowDefinition flow = new CpsFlowDefinition(pipelineScript,false);
         WorkflowJob workflowJob = j.createProject(WorkflowJob.class,"pipeline-job");
         workflowJob.setDefinition(flow);
         WorkflowRun result = workflowJob.scheduleBuild2(0).get();
