f8625199972ddf57108e37e02966c565e526fd64
Charles Moulliard <ch007m@gmail.com>
2021-01-06 15:10:47 +0100

    Create a dummy shared lib

---
+++ src/main/groovy/org/mytools/Tools.groovy
@@ -0,0 +1,14 @@
+package main.groovy.org.mytools
+
+class Tools implements Serializable {
+    private static final long serialVersionUID
+
+    def steps
+    Tools(steps) {
+        this.steps = steps
+    }
+
+    void myEcho(String msg) {
+        steps.echo msg
+    }
+}
