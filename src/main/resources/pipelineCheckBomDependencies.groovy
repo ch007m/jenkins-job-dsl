19a97519acc78f7ffc8225f7d409e31d9de3a4da
Charles Moulliard <ch007m@gmail.com>
2021-01-08 18:11:46 +0100

    Add pipeline definition using groovy shared lib

---
+++ src/main/resources/pipelineCheckBomDependencies.groovy
@@ -0,0 +1,60 @@
+@Library('snowdrop-lib') _
+
+// Global variables can be only set using the @Field pattern
+import groovy.transform.Field
+@Field def variable
+
+pipeline {
+    agent any
+
+    //ansiColor('xterm') {
+    //  echo "TERM=${env.TERM}"
+    //}
+    //sh "printenv"
+
+    environment {
+        PIPELINE_LOG_LEVEL = 'INFO'
+    }
+
+    options {
+        ansiColor('xterm')
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                gitCheckout(repo: "https://github.com/snowdrop/spring-boot-bom.git", branch: "2.3.6.Alpha2")
+            }
+        }
+
+        stage('Clean up pom.xml file and backup it') {
+            steps {
+                renamePomFile(ext: '.bk')
+                removeDependencyManagementTag()
+            }
+        }
+
+        stage('Check dependencies tree') {
+            steps {
+                mavenBuild(dependencyTree: true, compile: false)
+            }
+        }
+
+        stage('Check if we have git diff') {
+            steps {
+                mavenBuild(dependencyTree: true, compile: false)
+                restorePomFile(ext: '.bk', remove: true)
+
+                script {
+                    def status = gitstatus()
+                    if (status.contains("nothing to commit, working tree clean")) {
+                        log(level: 'WARN', text: "No Git difference :-)")
+                    } else {
+                        log(level: 'ERROR', text: "Git difference observed '${status}'. Consult the report.txt file under the workspace")
+                        sh "exit 1"
+                    }
+                }
+            }
+        }
+    }
+}
